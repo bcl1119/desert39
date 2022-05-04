4db48332b18ec23a9093fd0e3e7e04c2e93fe7a6
bcl1119 <jh11291125@gmail.com>
2022-05-04 14:18:17 +0900

    add header

--- html/Brand/script.js
+++ html/Brand/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     function animation(){
-      $(".roll, .line-up, .slide-left, .slide-up").each(function(){
+      $(".roll, .line-up, .slide-left").each(function(){
           var winh=$(window).height();
           var scrt = $(document).scrollTop();
           set = $(this).offset().top
@@ -16,4 +16,30 @@ $(document).ready(function(){
     $(window).scroll(function(){
         animation();
     });
+
+    var jbTime = 600;
+
+    function animation2(){
+      $(".slide-up").each(function(index, item){
+        let a=$(this)
+          var winh=$(window).height();
+          var scrt = $(document).scrollTop();
+            set = $(this).offset().top
+            if(scrt >= set - winh * 0.7){
+              setTimeout(function(){
+                a.addClass("active")
+                console.log(index*100)
+              }, index * 100);    
+            }else{
+              setTimeout(function(){
+                a.removeClass("active")
+              }, index * 10);    
+          }//if else
+    });
+    };
+    
+    animation2();
+    $(window).scroll(function(){
+      animation2();
+  });
 });
